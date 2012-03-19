a104f7d5d46d2589b21c76f34dc51afc5dc137c6
bsv798 <bsv798@gmail.com>
2012-03-19 15:13:23 +0600

    Change vrachInfo version

--- muzdrav/vrachInfo/config/ru/nkz/ivcgzo/configuration.java
+++ muzdrav/vrachInfo/config/ru/nkz/ivcgzo/configuration.java
@@ -1,7 +1,7 @@
 package ru.nkz.ivcgzo;
 
 public class configuration {
-	public static final int thrPort = 9094;
+	public static final int thrPort = 55001;
 	public static final int appId = 1;
 	public static final String appName = "Информация о персонале больницы";
 	public static final String appVersion = "0.0.0.0";
