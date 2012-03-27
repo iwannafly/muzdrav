a6ea7b8993a357d3d227f9255b9353c36335c4e3
bsv798 <bsv798@gmail.com>
2012-03-27 09:01:06 +0600

    osm serv impl

--- muzdrav/osm/server/src/ru/nkz/ivcgzo/serverOsm/ServerOsm.java
+++ muzdrav/osm/server/src/ru/nkz/ivcgzo/serverOsm/ServerOsm.java
@@ -1,5 +1,11 @@
 package ru.nkz.ivcgzo.serverOsm;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.apache.thrift.TException;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadedSelectorServer;
@@ -7,18 +13,31 @@ import org.apache.thrift.server.TThreadedSelectorServer.Args;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 
 import ru.nkz.ivcgzo.configuration;
+import ru.nkz.ivcgzo.serverManager.common.AutoCloseableResultSet;
 import ru.nkz.ivcgzo.serverManager.common.ISqlSelectExecutor;
 import ru.nkz.ivcgzo.serverManager.common.ITransactedSqlExecutor;
 import ru.nkz.ivcgzo.serverManager.common.Server;
+import ru.nkz.ivcgzo.serverManager.common.thrift.TResultSetMapper;
 import ru.nkz.ivcgzo.thriftOsm.ThriftOsm;
 import ru.nkz.ivcgzo.thriftOsm.ThriftOsm.Iface;
+import ru.nkz.ivcgzo.thriftOsm.ZapVr;
 
 public class ServerOsm extends Server implements Iface {
 	private TServer thrServ;
+	private final TResultSetMapper<ZapVr, ZapVr._Fields> rsmZapVr;
+	private final Class<?>[] zapVrTypes; 
 
 	public ServerOsm(ISqlSelectExecutor sse, ITransactedSqlExecutor tse) {
 		super(sse, tse);
-		// TODO Auto-generated constructor stub
+		
+		rsmZapVr = new TResultSetMapper<>(ZapVr.class, "npasp", "pcod_sp", "cdol", "fio_vr", "vidp", "timepn", "datapt", "fam", "im", "ot", "ot", "ot", "ot");
+		zapVrTypes = new Class<?>[] {Integer.class, Integer.class, String.class, String.class, Integer.class, Time.class, Timestamp.class, String.class, String.class, String.class, String.class, String.class, String.class};
+		try {
+			getZapVr(6, "3", System.currentTimeMillis());
+		} catch (TException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -44,6 +63,15 @@ public class ServerOsm extends Server implements Iface {
 	public void stop() {
 		if (thrServ != null)
 			thrServ.stop();
+	}
+
+	@Override
+	public List<ZapVr> getZapVr(int idvr, String cdol, long datap) throws TException {
+		try (AutoCloseableResultSet acrs = sse.execPreparedQuery("SELECT pat.npasp, tal.pcod_sp, tal.cdol, 'врач' as fio_vr, tal.vidp, tal.timepn, tal.datapt, pat.fam, pat.im, pat.ot, pat.ot, pat.ot, pat.ot FROM e_talon tal JOIN patient pat ON (pat.npasp = tal.npasp) WHERE (tal.pcod_sp = ?) AND (tal.cdol = ?) AND (tal.datap = ?)", idvr, cdol, new Date(datap))) {
+			return rsmZapVr.mapToList(acrs.getResultSet());
+		} catch (SQLException e) {
+			throw new TException(e);
+		}
 	}
 
 }
--- muzdrav/osm/thrift/osm.thrift
+++ muzdrav/osm/thrift/osm.thrift
@@ -2,5 +2,31 @@ namespace java ru.nkz.ivcgzo.thriftOsm
 
 include "../../../common/thrift/kmiacServer.thrift"
 
+/**
+ * Запись на пациента в регистратуре
+ */
+struct ZapVr{
+	1: i32 npasp;
+	2: i32 idvr;
+	3: string cdol;
+	4: string fio_vr;
+	5: i32 vid_p; /*вид приема-первичность*/
+	6: i64 timepn; /*время начала приема*/
+	7: i64 datapt; /*Дата и время приема*/
+	8: string fam;
+	9: string im;
+	10: string oth;
+	11: string cpos;
+	12: string serpolis;
+	13: string nompolis;
+}
+
+/**
+ * 
+ */
 service ThriftOsm extends kmiacServer.KmiacServer {
+	/**
+	 * Получение списка записанных на прием на заданную дату.
+	 */
+	list<ZapVr> getZapVr(1: i32 idvr, 2: string cdol, 3: i64 datap);
 }
