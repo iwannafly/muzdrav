6e8ab1576c02ffb3fefa01c47a517c7219b501c5
krechetova <NASTYA@(none)>
2012-03-27 13:11:24 +0700

    sozdanie formy s tablicei

--- muzdrav/osm/client/src/ru/nkz/ivcgzo/clientOsm/MainForm.java
+++ muzdrav/osm/client/src/ru/nkz/ivcgzo/clientOsm/MainForm.java
@@ -2,9 +2,12 @@ package ru.nkz.ivcgzo.clientOsm;
 
 import java.awt.EventQueue;
 import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.swing.JFrame;
 
+import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
@@ -14,12 +17,22 @@ import org.apache.thrift.transport.TTransportException;
 import ru.nkz.ivcgzo.configuration;
 import ru.nkz.ivcgzo.clientManager.common.Client;
 import ru.nkz.ivcgzo.clientManager.common.ConnectionManager;
+import ru.nkz.ivcgzo.clientManager.common.swing.CustomTable;
 import ru.nkz.ivcgzo.thriftCommon.kmiacServer.UserAuthInfo;
 import ru.nkz.ivcgzo.thriftOsm.ThriftOsm;
+import ru.nkz.ivcgzo.thriftOsm.ZapVr;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class MainForm extends Client {
 	private ThriftOsm.Client tcl;
 	private JFrame frame;
+	private CustomTable<ZapVr, ZapVr._Fields> table;
 
 	/**
 	 * Launch the application.
@@ -62,6 +75,34 @@ public class MainForm extends Client {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JButton btnNewButton = new JButton("Выбор");
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE)
+						.addComponent(btnNewButton))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(btnNewButton)
+					.addContainerGap())
+		);
+		
+		table = new CustomTable<>(false, true, ZapVr.class, ZapVr._Fields.values(), 0,"Внутр.номер",7, "Фамилия", 8, "Имя");
+		table.setFillsViewportHeight(true);
+		scrollPane.setViewportView(table);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 
 	@Override
@@ -83,7 +124,12 @@ public class MainForm extends Client {
 	public void onConnect(ru.nkz.ivcgzo.thriftCommon.kmiacServer.KmiacServer.Client conn) {
 		if (conn instanceof ThriftOsm.Client) {
 			tcl = (ThriftOsm.Client) conn;
+			try {
+				table.setData(tcl.getZapVr(6, "3", SimpleDateFormat.getDateInstance().parse("27.03.2012").getTime()));
+			} catch (TException | ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-
 }
