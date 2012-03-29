216dee7480cfb944ba8c63605996c711c7558dc6
krechetova <krechetova@ivcgzo.ru>
2012-03-29 11:41:33 +0700

    Thrift.Forms

--- muzdrav/osm/thrift/osm.thrift
+++ muzdrav/osm/thrift/osm.thrift
@@ -16,6 +16,30 @@ struct ZapVr{
 	8: string nompolis;
 }
 
+struct Pvizit {
+	1: i32 npasp;
+	2: i32 idzab;
+	3: i32 idvr;
+	4: string cdol;
+	5: i64 datap;
+	6: string cobr;
+	7: i32 ish;
+	8: i32 rez_t;
+	9: i32 talon;
+	10: string ztext;
+	11: i32 cod_sp;
+}
+
+struct PvizitAmb {
+	1: i32 npasp;
+	2: i32 idzab;
+	3: i32 idpos;
+	4: i64 datap;
+	5: i32 cod_sp;
+	6: i32 rez;
+	7: i32 opl;
+}
+
 /**
  * 
  */
@@ -24,4 +48,10 @@ service ThriftOsm extends kmiacServer.KmiacServer {
 	 * Получение списка записанных на прием на заданную дату.
 	 */
 	list<ZapVr> getZapVr(1: i32 idvr, 2: string cdol, 3: i64 datap);
+	void AddPvizit(1: i32 zab);
+	void UpdatePvizit(1: i32 zab);
+	void DeletePvizit(1: i32 zab);
+	void AddPvizitAmb(1: i32 pos);
+	void UpdatePvizitAmb(1: i32 pos);
+	void DeletePvizitAmb(1: i32 pos);
 }
