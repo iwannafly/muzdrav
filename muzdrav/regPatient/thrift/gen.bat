6fd6c72d0a3c8735045b54b0058b7a7f2d4794b1
iwannafly <suckmyluck13@gmail.com>
2012-04-17 17:50:51 +0700

    delete fucking thrift autogenered files from regPatient

--- muzdrav/regPatient/thrift/gen.bat
+++
@@ -1,5 +0,0 @@
-RMDIR /S /Q gen-java
-"..\..\..\..\bin\thrift.exe" --gen java "regPatient.thrift"
-"..\..\..\..\bin\thrift.exe" --gen java "..\..\..\common\thrift\kmiacServer.thrift"
-"..\..\..\..\bin\thrift.exe" --gen java "..\..\..\common\thrift\classifier.thrift"
-pause
