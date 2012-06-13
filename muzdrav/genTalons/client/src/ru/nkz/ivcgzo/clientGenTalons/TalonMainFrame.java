be8f907d85847119aced057b35907365f3d54c4c
netyaga <Netyaga@NETYAGA.(none)>
2012-06-13 08:55:53 +0600

    clientGenTalons

--- muzdrav/genTalons/client/src/ru/nkz/ivcgzo/clientGenTalons/TalonMainFrame.java
+++ muzdrav/genTalons/client/src/ru/nkz/ivcgzo/clientGenTalons/TalonMainFrame.java
@@ -7,10 +7,30 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JScrollBar;
 import javax.swing.border.TitledBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTabbedPane;
+
+import ru.nkz.ivcgzo.thriftGenTalon.Calendar;
+import ru.nkz.ivcgzo.thriftGenTalon.Ndv;
+import ru.nkz.ivcgzo.thriftGenTalon.Norm;
+import ru.nkz.ivcgzo.thriftGenTalon.Nrasp;
+import ru.nkz.ivcgzo.thriftGenTalon.Rasp;
+import ru.nkz.ivcgzo.thriftGenTalon.Spec;
+import ru.nkz.ivcgzo.thriftGenTalon.Talon;
+import ru.nkz.ivcgzo.thriftGenTalon.ThriftGenTalons;
+import ru.nkz.ivcgzo.thriftGenTalon.ThriftGenTalons.Iface;
+import ru.nkz.ivcgzo.thriftGenTalon.Vidp;
+import ru.nkz.ivcgzo.thriftGenTalon.Vrach;
 
 public class TalonMainFrame extends JFrame {
 
@@ -18,23 +38,26 @@ public class TalonMainFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final String lineSep = System.lineSeparator();
 	private JPanel contentPane;
+	private JTree treevrach;
+	private StringBuilder sb;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TalonMainFrame frame = new TalonMainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					TalonMainFrame frame = new TalonMainFrame();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
@@ -42,24 +65,82 @@ public class TalonMainFrame extends JFrame {
 	public TalonMainFrame() {
 		setTitle("Электронная регистратура");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 423);
+		setBounds(1, 1, 958, 690); //(x,y,width,height)
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "\u0421\u043F\u0438\u0441\u043E\u043A \u0432\u0440\u0430\u0447\u0435\u0439", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JTabbedPane tbMain = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 245, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(187, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(tbMain, GroupLayout.DEFAULT_SIZE, 685, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 379, Short.MAX_VALUE)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 646, Short.MAX_VALUE)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(tbMain, GroupLayout.DEFAULT_SIZE, 635, Short.MAX_VALUE))
 		);
+		
+		JPanel panel_1 = new JPanel();
+		tbMain.addTab("New tab", null, panel_1, null);
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 680, Short.MAX_VALUE)
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 618, Short.MAX_VALUE)
+		);
+		panel_1.setLayout(gl_panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		tbMain.addTab("New tab", null, panel_2, null);
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 680, Short.MAX_VALUE)
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 618, Short.MAX_VALUE)
+		);
+		panel_2.setLayout(gl_panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		tbMain.addTab("New tab", null, panel_3, null);
+		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
+		gl_panel_3.setHorizontalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 680, Short.MAX_VALUE)
+		);
+		gl_panel_3.setVerticalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 618, Short.MAX_VALUE)
+		);
+		panel_3.setLayout(gl_panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		tbMain.addTab("New tab", null, panel_4, null);
+		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
+		gl_panel_4.setHorizontalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 680, Short.MAX_VALUE)
+		);
+		gl_panel_4.setVerticalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 618, Short.MAX_VALUE)
+		);
+		panel_4.setLayout(gl_panel_4);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_panel = new GroupLayout(panel);
@@ -72,9 +153,39 @@ public class TalonMainFrame extends JFrame {
 				.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
 		);
 		
-		JTree tree = new JTree();
-		scrollPane.setViewportView(tree);
+		treevrach = new JTree(createNodes());
+		scrollPane.setViewportView(treevrach);
+		treevrach.setShowsRootHandles(true);
+		treevrach.setRootVisible(false);
+		DefaultTreeCellRenderer renderer =  (DefaultTreeCellRenderer) treevrach.getCellRenderer();
 		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
+	}
+
+	private DefaultMutableTreeNode createNodes() {
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Корень зла");
+		
+		try {
+			for (Spec spec : MainForm.tcl.getAllSpecForPolikliniki(MainForm.authInfo.cpodr))
+				root.add(new VrachTreeNode(spec));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return root;
+	}
+
+	class VrachTreeNode extends DefaultMutableTreeNode {
+		private static final long serialVersionUID = 4212592425962984738L;
+		private Spec spec;
+		
+		public VrachTreeNode(Spec spec) {
+			this.spec = spec;
+		}
+		
+		@Override
+		public String toString() {
+			return spec.getName();
+		}
 	}
 }
