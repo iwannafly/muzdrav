de03a87cef60699d5945cc525648f0708b1025d5
krechetova <krechetova@ivcgzo.nkz.ru>
2012-10-08 07:37:21 +0600

    Osm

--- muzdrav/lab/thrift/lab.thrift
+++
@@ -1,7 +0,0 @@
-namespace java ru.nkz.ivcgzo.thriftLab
-
-include "../../../common/thrift/classifier.thrift"
-include "../../../common/thrift/kmiacServer.thrift"
-
-service ThriftLab extends kmiacServer.KmiacServer {
-}
