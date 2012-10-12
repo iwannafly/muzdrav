f51e83207430cc77261d5bd401c825673009b81f
iwannafly <suckmyluck13@gmail.com>
2012-10-12 08:09:17 +0700

    fix conflict

--- muzdrav/hospital/client/src/ru/nkz/ivcgzo/clientHospital/MainFrame.java
+++ muzdrav/hospital/client/src/ru/nkz/ivcgzo/clientHospital/MainFrame.java
@@ -1837,6 +1837,7 @@ public class MainFrame extends JFrame {
     }
 
     private void setZaklPanelGroupLayout() {
+
         GroupLayout glPZakl = new GroupLayout(pZakl);
         glPZakl.setHorizontalGroup(
             glPZakl.createParallelGroup(Alignment.LEADING)
