3c64d2929cb3bc189c9d0a0f467d56781adfd975
krechetova <krechetova@ivcgzo.nkz.ru>
2012-10-16 16:23:18 +0600

    osm

--- muzdrav/osm/client/src/ru/nkz/ivcgzo/clientOsm/Vvod.java
+++ muzdrav/osm/client/src/ru/nkz/ivcgzo/clientOsm/Vvod.java
@@ -864,22 +864,24 @@ public class Vvod extends JFrame {
 					.addGap(0))
 		);
 		
-		tblDiag = new CustomTable<>(false, true, PdiagAmb.class, 7, "Дата", 3, "Код МКБ");
+		tblDiag = new CustomTable<>(true, true, PdiagAmb.class, 7, "Дата", 3, "Код МКБ");
 		tblDiag.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent  e) {
 				if (e.getClickCount() == 2)
-					if (tblDiag.getSelectedItem() != null) {
-						StringClassifier res = ConnectionManager.instance.showMkbTreeForm("Диагнозы", tblDiag.getSelectedItem().diag);
-						if (res != null) {
-							tblDiag.getSelectedItem().setDiag(res.pcod);
-							tbDiagOpis.setText(res.name);
-							tblDiag.updateSelectedItem();
-						}
+					if (tblDiag.getSelectedItem().diag != null)
+						if (tblDiag.getSelectedColumn() == 1) {
+							StringClassifier res = ConnectionManager.instance.showMkbTreeForm("Диагнозы", tblDiag.getSelectedItem().diag);
+							if (res != null) {
+								tblDiag.getSelectedItem().setDiag(res.pcod);
+								tbDiagOpis.setText(res.name);
+								tblDiag.updateSelectedItem();
+							}
 					}
 			}
 		});
 		tblDiag.setDateField(0);
+		tblDiag.setEditableFields(false, 1);
 		tblDiag.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
@@ -2182,7 +2184,7 @@ public class Vvod extends JFrame {
 		
 		tblPos = new CustomTable<>(true, false, PvizitAmb.class, 3, "Дата", 19, "ФИО врача", 5, "Должность");
 		tblPos.setDateField(0);
-		tblPos.setEditableFields(false, 1, 2);
+		tblPos.setEditableFields(false, 2);
 		tblPos.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 		@Override
 		public void valueChanged(ListSelectionEvent arg0) {
--- muzdrav/osm/server/src/ru/nkz/ivcgzo/serverOsm/ServerOsm.java
+++ muzdrav/osm/server/src/ru/nkz/ivcgzo/serverOsm/ServerOsm.java
@@ -2177,7 +2177,7 @@ acrs = sse.execPreparedQuery("select s_vrach.fam,s_vrach.im,s_vrach.ot from s_us
 	@Override
 	public int AddPmer(Pmer pm) throws KmiacServerException, TException {
 		try (SqlModifyExecutor sme = tse.startTransaction()) {
-			sme.execPreparedT("insert into p_mer (npasp, diag, pmer, pdat, fdat, cod_sp, dataz, prichina, rez, cdol, dnl, dkl, lpu, ter, cpol) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ", true, pm, pmerTypes, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+			sme.execPreparedT("insert into p_mer (npasp, diag, cpol, dataz) values (?, ?, ?, ?) ", true, pm, pmerTypes, 1, 2, 15, 7);
 			int id = sme.getGeneratedKeys().getInt("id");
 			sme.setCommit();
 			return id;
@@ -2670,7 +2670,7 @@ acrs = sse.execPreparedQuery("select s_vrach.fam,s_vrach.im,s_vrach.ot from s_us
 			return rsmIntClas.mapToList(acrs.getResultSet());
 		} catch (SQLException e) {
 			((SQLException) e.getCause()).printStackTrace();
-			throw new KmiacServerException();
+			throw new KmiacServerException(); 
 		}
 	}
 
