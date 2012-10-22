fc07b942244d79a8108733992e6499d31cac2eb5
bsv798 <bsv798@gmail.com>
2012-10-22 17:11:50 +0600

    custom table and its components fixes

--- common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/swing/TableNumberEditor.java
+++ common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/swing/TableNumberEditor.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.DefaultCellEditor;
@@ -36,6 +38,12 @@ public class TableNumberEditor extends DefaultCellEditor {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				ctb.dispatchEvent(new KeyEvent(ctb, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_ENTER, KeyEvent.CHAR_UNDEFINED));
+			}
+		});
+		txt.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				stopCellEditing();
 			}
 		});
 		renderer = new TableNumberRenderer();
