91edea620c04ac0c94405e7fc8ff93042b5b5a6c
bsv798 <bsv798@gmail.com>
2012-11-16 18:17:07 +0600

    add new project autoProc

---
+++ muzdrav/autoProc/thrift/autoProc.thrift
@@ -0,0 +1,7 @@
+namespace java ru.nkz.ivcgzo.thriftServerAutoProc
+
+include "../../../common/thrift/classifier.thrift"
+include "../../../common/thrift/kmiacServer.thrift"
+
+service ThriftServerAutoProc extends kmiacServer.KmiacServer {
+}
