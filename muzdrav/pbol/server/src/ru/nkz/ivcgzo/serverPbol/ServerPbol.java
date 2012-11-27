50938ab02f72e7026a4f04192d525942d3290acc
krechetova <krechetova@ivcgzo.nkz.ru>
2012-11-27 14:41:57 +0600

    pbol rename fix

--- muzdrav/pbol/server/src/ru/nkz/ivcgzo/serverPbol/ServerPbol.java
+++ muzdrav/pbol/server/src/ru/nkz/ivcgzo/serverPbol/ServerPbol.java
@@ -1,4 +1,4 @@
-package ru.nkz.ivcgzo.serverPbol1;
+package ru.nkz.ivcgzo.serverPbol;
 
 import java.sql.Date;
 import java.util.List;
@@ -18,12 +18,12 @@ import ru.nkz.ivcgzo.thriftPbol.Pbol;
 import ru.nkz.ivcgzo.thriftPbol.ThriftPbol;
 import ru.nkz.ivcgzo.thriftPbol.ThriftPbol.Iface;
 
-public class ServerPbol1 extends Server implements Iface {
+public class ServerPbol extends Server implements Iface {
 	private TServer thrServ;
 	private final TResultSetMapper<Pbol, Pbol._Fields> rsmPbol;
 	private final Class<?>[] pbolTypes; 
 
-	public ServerPbol1(ISqlSelectExecutor sse, ITransactedSqlExecutor tse) {
+	public ServerPbol(ISqlSelectExecutor sse, ITransactedSqlExecutor tse) {
 		super(sse, tse);
 		rsmPbol = new TResultSetMapper<>(Pbol.class, "id",          "id_obr",      "id_gosp",     "npasp",       "bol_l",       "s_bl", 	"po_bl",    "pol",         "vozr",        "nombl", 	    "cod_sp",      "cdol",       "pcod",        "dataz");
 		pbolTypes = new Class<?>[] {                 Integer.class, Integer.class, Integer.class, Integer.class, Integer.class, Date.class, Date.class, Integer.class, Integer.class, String.class, Integer.class, String.class, Integer.class, Date.class};
