4adfd4a97ec993f51b05e93c644edc5982a12ebf
perehodova <perehodova61@mail.ru>
2012-11-29 14:03:39 +0600

    FocusTravesal delete

--- muzdrav/hospital/client/src/ru/nkz/ivcgzo/clientHospital/MainFrame.java
+++ muzdrav/hospital/client/src/ru/nkz/ivcgzo/clientHospital/MainFrame.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
