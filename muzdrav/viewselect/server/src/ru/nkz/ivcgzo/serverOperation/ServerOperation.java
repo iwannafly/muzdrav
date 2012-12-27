56637abfe817760d4968580154fafd12994a2e70
iwannafly <suckmyluck13@gmail.com>
2012-12-27 14:32:23 +0600

    Add some fields to serverOperation

--- muzdrav/viewselect/server/src/ru/nkz/ivcgzo/serverOperation/ServerOperation.java
+++ muzdrav/viewselect/server/src/ru/nkz/ivcgzo/serverOperation/ServerOperation.java
@@ -1,6 +1,8 @@
 package ru.nkz.ivcgzo.serverOperation;
 
 import java.io.File;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
 import org.apache.log4j.Level;
@@ -16,13 +18,78 @@ import ru.nkz.ivcgzo.configuration;
 import ru.nkz.ivcgzo.serverManager.common.ISqlSelectExecutor;
 import ru.nkz.ivcgzo.serverManager.common.ITransactedSqlExecutor;
 import ru.nkz.ivcgzo.serverManager.common.Server;
+import ru.nkz.ivcgzo.serverManager.common.thrift.TResultSetMapper;
 import ru.nkz.ivcgzo.thriftCommon.kmiacServer.KmiacServerException;
 import ru.nkz.ivcgzo.thriftOperation.*;
 import ru.nkz.ivcgzo.thriftOperation.ThriftOperation.Iface;
 
 public class ServerOperation extends Server implements Iface {
-    private static Logger log = Logger.getLogger(ServerOperation.class.getName());
+    private static final Logger log = Logger.getLogger(ServerOperation.class.getName());
     private TServer tServer;
+    private TResultSetMapper<Operation, Operation._Fields> rsmOperation;
+    private TResultSetMapper<OperationComplication,
+            OperationComplication._Fields> rsmOperationComplication;
+    private TResultSetMapper<OperationPaymentFund,
+            OperationPaymentFund._Fields> rsmOperationPaymentFund;
+    private TResultSetMapper<Anesthesia, Anesthesia._Fields> rsmAnesthesia;
+    private TResultSetMapper<AnesthesiaComplication,
+            AnesthesiaComplication._Fields> rsmAnesthesiaComplication;
+    private TResultSetMapper<AnesthesiaPaymentFund,
+            AnesthesiaPaymentFund._Fields> rsmAnesthesiaPaymentFund;
+
+    private static final String[] OPERATION_FIELD_NAMES = {
+            "id", "vid_st", "cotd", "id_gosp", "npasp", "pcod", "name_oper", "date", "vrem",
+            "pred_ep", "op_oper", "material", "dlit", "dataz"
+    };
+    private static final String[] OPERATION_COMPLICATION_FIELD_NAMES = {
+            "id", "id_oper", "name_osl", "pcod", "dataz"
+    };
+    private static final String[] OPERATION_PAYMENT_FUND_FIELD_NAMES = {
+            "id", "id_oper", "pcod", "dataz"
+    };
+    private static final String[] ANESTHESIA_FIELD_NAMES = {
+            "id", "vid_st", "cotd", "id_gosp", "npasp", "id_oper", "pcod", "name_an", "date",
+            "vrem", "dataz"
+    };
+    private static final String[] ANESTHESIA_COMPLICATION_FIELD_NAMES = {
+            "id", "id_anast", "name", "pcod", "dataz"
+    };
+    private static final String[] ANESTHESIA_PAYMENT_FUND_FIELD_NAMES = {
+            "id", "id_anast", "pcod", "dataz"
+    };
+
+    private static final Class<?>[] OPERATION_TYPES ={
+//          id             vid_st         cotd           id_gosp        npasp
+            Integer.class, Integer.class, Integer.class, Integer.class, Integer.class,
+//          pcod           name_oper     date        vrem        pred_ep       op_oper
+            Integer.class, String.class, Date.class, Time.class, String.class, String.class,
+//          material      dlit           dataz
+            String.class, Integer.class, Date.class
+    };
+    private static final Class<?>[] OPERATION_COMPLICATION_TYPES ={
+//          id           id_oper          name_osl      pcod           dataz
+            Integer.class, Integer.class, String.class, Integer.class, Date.class
+    };
+    private static final Class<?>[] OPERATION_PAYMENT_FUND_TYPES ={
+//          id             id_oper        pcod           dataz
+            Integer.class, Integer.class, Integer.class, Date.class
+    };
+    private static final Class<?>[] ANESTHESIA_TYPES ={
+//          id             vid_st         cotd           id_gosp        npasp
+            Integer.class, Integer.class, Integer.class, Integer.class, Integer.class,
+//          id_oper        pcod           name_an       date        vrem        dataz
+            Integer.class, Integer.class, String.class, Date.class, Time.class, Date.class
+    };
+    private static final Class<?>[] ANESTHESIA_COMPLICATION_TYPES ={
+//          id             id_anast       name          pcod           dataz
+            Integer.class, Integer.class, String.class, Integer.class, Date.class
+    };
+
+    private static final Class<?>[] ANESTHESIA_PAYMENT_FUND_TYPES ={
+//          id             id_anast       pcod           dataz
+            Integer.class, Integer.class, Integer.class, Date.class
+    };
+
 
     public ServerOperation(final ISqlSelectExecutor sse, final ITransactedSqlExecutor tse) {
         super(sse, tse);
@@ -31,6 +98,17 @@ public class ServerOperation extends Server implements Iface {
         String manPath = new File(this.getClass().getProtectionDomain().getCodeSource()
             .getLocation().getPath()).getParentFile().getParentFile().getAbsolutePath();
         DOMConfigurator.configure(new File(manPath, "log4j.xml").getAbsolutePath());
+
+        rsmOperation = new TResultSetMapper<>(Operation.class, OPERATION_FIELD_NAMES);
+        rsmOperationComplication = new TResultSetMapper<>(OperationComplication.class,
+                OPERATION_COMPLICATION_FIELD_NAMES);
+        rsmOperationPaymentFund = new TResultSetMapper<>(OperationPaymentFund.class, 
+                OPERATION_PAYMENT_FUND_FIELD_NAMES);
+        rsmAnesthesia = new TResultSetMapper<>(Anesthesia.class, ANESTHESIA_FIELD_NAMES);
+        rsmAnesthesiaComplication = new TResultSetMapper<>(AnesthesiaComplication.class,
+                ANESTHESIA_COMPLICATION_FIELD_NAMES);
+        rsmAnesthesiaPaymentFund = new TResultSetMapper<>(AnesthesiaPaymentFund.class,
+                ANESTHESIA_PAYMENT_FUND_FIELD_NAMES);
     }
 
     @Override
@@ -47,7 +125,7 @@ public class ServerOperation extends Server implements Iface {
     @Override
     public void stop() {
         tServer.stop();
-        log.log(Level.INFO, "lab server stopped");
+        log.log(Level.INFO, "operation server stopped");
     }
 
     @Override
