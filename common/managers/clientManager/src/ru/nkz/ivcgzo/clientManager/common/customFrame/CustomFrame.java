ba7860bb1c60b28f7dedc05667b88831eff8ca54
bsv798 <bsv798@gmail.com>
2013-01-23 10:19:00 +0700

    redmine help barely working

--- common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/customFrame/CustomFrame.java
+++ common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/customFrame/CustomFrame.java
@@ -65,7 +65,7 @@ public class CustomFrame extends JFrame {
 		mmHelp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				helpFrm.setVisible(true);
+				helpFrm.showHelp("http://cds:3000/projects/sysdev/wiki/%D0%9D%D0%B0%D1%87%D0%B0%D0%BB%D0%BE_%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B_%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0_msysgit_%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D1%8B");
 			}
 		});
 		menuBar.add(mmHelp);
--- common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/customFrame/HelpForm.java
+++ common/managers/clientManager/src/ru/nkz/ivcgzo/clientManager/common/customFrame/HelpForm.java
@@ -1,13 +1,53 @@
 package ru.nkz.ivcgzo.clientManager.common.customFrame;
 
+import java.awt.BorderLayout;
+import java.util.Enumeration;
+
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
 
 public class HelpForm extends JDialog {
 	private static final long serialVersionUID = -3095501803314345336L;
+	private JEditorPane editorPane;
 	
 	public HelpForm() {
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setTitle("Просмотр справки");
+		setBounds(100, 100, 800, 600);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		editorPane = new JEditorPane();
+		scrollPane.setViewportView(editorPane);
+		
+	}
+	
+	public void showHelp(String url) {
+		try {
+			editorPane.setDocument(new HTMLDocument());
+			editorPane.setPage(url);
+			HTMLDocument doc = (HTMLDocument) editorPane.getDocument();
+			while (doc.getElement("top-menu") == null) {
+				Thread.sleep(100);
+			}
+			doc.removeElement(doc.getElement("top-menu"));
+			doc.removeElement(doc.getElement("header"));
+			doc.removeElement(doc.getElement("sidebar"));
+			doc.removeElement(doc.getElement(doc.getDefaultRootElement(), HTML.Attribute.CLASS, "contextual"));
+			doc.removeElement(doc.getElement(doc.getDefaultRootElement(), HTML.Attribute.CLASS, "attachments"));
+			doc.removeElement(doc.getElement("ajax-indicator"));
+			Enumeration<?> sen = doc.getStyleNames();
+			while (sen.hasMoreElements())
+				doc.removeStyle(((String) sen.nextElement()));
+			setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Fuck you.");
+		}
 	}
 }
