dc708f52dac797774087c0bf271138593b6e55eb
krechetova <krechetova@ivcgzo.nkz.ru>
2013-02-06 15:05:00 +0600

    file gen.bat is changed

--- muzdrav/print/server/src/ru/nkz/ivcgzo/serverPrint/ServerPrint.java
+++ muzdrav/print/server/src/ru/nkz/ivcgzo/serverPrint/ServerPrint.java
@@ -11,8 +11,15 @@ import ru.nkz.ivcgzo.configuration;
 import ru.nkz.ivcgzo.serverManager.common.ISqlSelectExecutor;
 import ru.nkz.ivcgzo.serverManager.common.ITransactedSqlExecutor;
 import ru.nkz.ivcgzo.serverManager.common.Server;
+import ru.nkz.ivcgzo.thriftCommon.kmiacServer.KmiacServerException;
+import ru.nkz.ivcgzo.thriftPrint.KartaBer;
+import ru.nkz.ivcgzo.thriftPrint.Napr;
+import ru.nkz.ivcgzo.thriftPrint.NaprKons;
+import ru.nkz.ivcgzo.thriftPrint.Protokol;
+import ru.nkz.ivcgzo.thriftPrint.SpravNetrud;
 import ru.nkz.ivcgzo.thriftPrint.ThriftPrint;
 import ru.nkz.ivcgzo.thriftPrint.ThriftPrint.Iface;
+import ru.nkz.ivcgzo.thriftPrint.Vypis;
 
 public class ServerPrint extends Server implements Iface {
 	private TServer thrServ;
@@ -47,6 +54,66 @@ public class ServerPrint extends Server implements Iface {
 		if (thrServ != null)
 			thrServ.stop();
 
+	}
+
+	@Override
+	public String printNapr(Napr na) throws KmiacServerException, TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printNaprKons(NaprKons nk) throws KmiacServerException,
+			TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printVypis(Vypis vp) throws KmiacServerException, TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printKek(int npasp, int pvizitId)
+			throws KmiacServerException, TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printProtokol(Protokol pk) throws KmiacServerException,
+			TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printMSK(int npasp) throws KmiacServerException, TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printAnamZab(int id_pvizit) throws KmiacServerException,
+			TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printSpravNetrud(SpravNetrud sn) throws KmiacServerException,
+			TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String printKartaBer(KartaBer kb) throws KmiacServerException,
+			TException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
