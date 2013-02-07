1207d09a2c828c1eb4e5fbab70e1330702fc9707
schigarev <kirill.shchigarev@gmail.com>
2013-02-07 09:02:30 +0600

    initial

--- muzdrav/outputInfo/client/src/ru/nkz/ivcgzo/clientOutPutInfo/StructPos.java
+++ muzdrav/outputInfo/client/src/ru/nkz/ivcgzo/clientOutPutInfo/StructPos.java
@@ -35,26 +35,100 @@ public class StructPos extends JPanel {
 	
 	public StructPos() {
 		JLabel label = new JLabel("Сведения о структуре посещений и использования рабочего времени");
+				
+		final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		JPanel panel = new JPanel();
+
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(114)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 303, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label))
+					.addContainerGap(129, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(label)
+					.addGap(37)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 392, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(46, Short.MAX_VALUE))
+		);
 		
 		JLabel label_1 = new JLabel("Период формирования:");
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-
-		final JRadioButton rbutP = new JRadioButton("По дате посещения");
-		final JRadioButton rbutB = new JRadioButton("По дате записи в базу");
-		butGroup.add(rbutP);
-		butGroup.add(rbutB);
-		rbutP.setSelected(true);
-				
-		final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Date1 = new CustomDateEditor();
 		Date1.setText("19092012");
+		JLabel label_3 = new JLabel("По");
 		Date2 = new CustomDateEditor();
 		Date2.setText("11102012");
 		
-		JLabel label_2 = new JLabel("С");
-		JLabel label_3 = new JLabel("По");
-		
 		JButton button = new JButton("Выполнить");
+		
+		JButton button_1 = new JButton("Закрыть");
+				
+				JLabel label_2 = new JLabel("С");
+		
+				final JRadioButton rbutP = new JRadioButton("По дате посещения");
+				butGroup.add(rbutP);
+				rbutP.setSelected(true);
+				final JRadioButton rbutB = new JRadioButton("По дате записи в базу");
+				butGroup.add(rbutB);
+				GroupLayout gl_panel = new GroupLayout(panel);
+				gl_panel.setHorizontalGroup(
+					gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(64)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+										.addComponent(label_3)
+										.addComponent(label_2))
+									.addGap(10)
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+										.addGroup(gl_panel.createSequentialGroup()
+											.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(Date2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(Date1, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE))
+											.addPreferredGap(ComponentPlacement.UNRELATED)
+											.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(button_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(button, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+										.addComponent(rbutP)
+										.addComponent(rbutB))
+									.addGap(30))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(label_1)
+									.addGap(195))))
+				);
+				gl_panel.setVerticalGroup(
+					gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(label_1)
+							.addGap(53)
+							.addComponent(rbutP)
+							.addGap(18)
+							.addComponent(rbutB)
+							.addGap(38)
+							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+								.addComponent(Date1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(label_2)
+								.addComponent(button))
+							.addGap(18)
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addComponent(label_3)
+								.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+									.addComponent(Date2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addComponent(button_1)))
+							.addContainerGap(145, Short.MAX_VALUE))
+				);
+				panel.setLayout(gl_panel);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
@@ -79,61 +153,6 @@ public class StructPos extends JPanel {
 			
 			}
 		});
-		
-		JButton button_1 = new JButton("Закрыть");
-
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(32)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(106)
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(label_1)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(label_2)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(Date1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addGap(18)
-									.addComponent(label_3)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(Date2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-									.addGap(8))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(button)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(button_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(rbutP)
-									.addComponent(rbutB))))
-						.addComponent(label))
-					.addContainerGap(175, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label)
-					.addGap(82)
-					.addComponent(label_1)
-					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-					.addComponent(rbutP)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(rbutB)
-					.addGap(37)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(Date2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(label_3)
-						.addComponent(label_2)
-						.addComponent(Date1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(41)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button)
-						.addComponent(button_1))
-					.addGap(130))
-		);
 		setLayout(groupLayout);
 		
 		JScrollPane scrollPane = new JScrollPane();
