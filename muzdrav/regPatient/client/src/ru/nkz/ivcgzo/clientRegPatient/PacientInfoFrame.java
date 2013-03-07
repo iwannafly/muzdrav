490ec1d5fde83c32a528c312cc1b0a3c9ec88ae6
netyaga <netyaga@ivcgzo.nkz.ru>
2013-03-07 11:07:39 +0600

    change GenReestr and regpatient

--- muzdrav/regPatient/client/src/ru/nkz/ivcgzo/clientRegPatient/PacientInfoFrame.java
+++ muzdrav/regPatient/client/src/ru/nkz/ivcgzo/clientRegPatient/PacientInfoFrame.java
@@ -456,9 +456,10 @@ public class PacientInfoFrame extends JFrame {
         tbMain.setFont(new Font("Tahoma", Font.PLAIN, 12));
         tbMain.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
-                if (tbMain.getSelectedIndex() == 0 || tbMain.getSelectedIndex() == 1 || tbMain.getSelectedIndex() == 2 || tbMain.getSelectedIndex() == 3 || tbMain.getSelectedIndex() == 4 || tbMain.getSelectedIndex() == 6) {
+                if (MainForm.authInfo.getCslu() == 1) 
+                 if (tbMain.getSelectedIndex() == 0 || tbMain.getSelectedIndex() == 1 || tbMain.getSelectedIndex() == 2 || tbMain.getSelectedIndex() == 3 || tbMain.getSelectedIndex() == 4 || tbMain.getSelectedIndex() == 6) 
                 	if (Id_gosp != null)SavePriemInfo();
-                }
+                 
                 if (tbMain.getSelectedIndex() == 1) {
                     changePatientLgotaInfo(curPatientId);
                     InfoForLgotaPatient();
